36b87596d4cbeb4206704ab16648a9cd204e5445
saurabh <rokr.saurabh31@gmail.com>
2020-08-11 00:29:55 +0530

    Updates

---
+++ precache-manifest.1a15b32dfe750c6f34314873f4955bb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44349bc95675479c584087f7ed23082",
+    "url": "/Reactapp/index.html"
+  },
+  {
+    "revision": "0cf85711e4176e60d3da",
+    "url": "/Reactapp/static/css/main.479abc78.chunk.css"
+  },
+  {
+    "revision": "daf7a6b263a55b0176b9",
+    "url": "/Reactapp/static/js/2.c487a060.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reactapp/static/js/2.c487a060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf85711e4176e60d3da",
+    "url": "/Reactapp/static/js/main.c7ded631.chunk.js"
+  },
+  {
+    "revision": "6e2d2906c0a65da14c01",
+    "url": "/Reactapp/static/js/runtime-main.50c7724a.js"
+  },
+  {
+    "revision": "75d7281e103e672250a7de958133ba96",
+    "url": "/Reactapp/static/media/weather.75d7281e.jpg"
+  }
+]);
